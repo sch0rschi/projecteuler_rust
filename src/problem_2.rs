f0a50a90474eabec8a2335656b290358f41b6641
sch0rschi <getschii@gmail.com>
2025-10-05 18:58:15 +0200

    problem 2

---
+++ src/problem_2.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut f_np  = 1;
+    let mut f_n = 2;
+    let mut sum = f_n.clone();
+
+    while f_n <= 4000000 {
+        let temp = f_n;
+        f_n += f_np;
+        f_np = temp;
+        if f_n % 2 == 0 {
+            sum += f_n;
+        }
+    }
+
+    println!("{}", sum);
+}
