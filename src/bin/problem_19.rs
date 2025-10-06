6edc1bf095d3221c3103de33c29cfa5c824aa46f
sch0rschi <getschii@gmail.com>
2025-10-06 11:59:31 +0200

    problem 19

---
+++ src/bin/problem_19.rs
@@ -0,0 +1,16 @@
+use chrono::{Datelike, NaiveDate, Weekday};
+
+fn main() {
+    let start = NaiveDate::from_ymd_opt(1901, 1, 1).unwrap();
+    let end = NaiveDate::from_ymd_opt(2000, 12, 31).unwrap();
+
+    let mut counter = 0;
+    let mut date = start.clone();
+    while date <= end {
+        if date.day() == 1 && date.weekday() == Weekday::Sun {
+            counter += 1;
+        }
+        date = date.succ_opt().unwrap()
+    }
+    println!("{}", counter);
+ }
