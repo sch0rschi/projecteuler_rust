f36f5889bfe16d1fe3974bc9225128527cc1e210
sch0rschi <getschii@gmail.com>
2025-10-06 18:25:23 +0200

    problem 21

---
+++ src/bin/problem_21.rs
@@ -0,0 +1,15 @@
+use projecteuler::divisors::proper_divisor_sum;
+
+fn main() {
+    let mut d: [i64; 10_000] = [0; 10_000];
+    let mut amicable_numbers_sum = 0;
+    for i in 1..10_000 {
+        let proper_divisor_sum = proper_divisor_sum(i);
+        d[i as usize] = proper_divisor_sum;
+        if proper_divisor_sum < i && d[proper_divisor_sum as usize] == i {
+            amicable_numbers_sum += i;
+            amicable_numbers_sum += proper_divisor_sum;
+        }
+    }
+    println!("{}", amicable_numbers_sum);
+}
---
+++ src/divisors.rs
@@ -0,0 +1,24 @@
+pub fn proper_divisor_sum(n: i64) -> i64 {
+    proper_divisors(n).iter().sum()
+}
+
+fn proper_divisors(n: i64) -> Vec<i64> {
+    let mut n_sqrt = n.isqrt();
+    let mut proper_divisors: Vec<i64> = Vec::new();
+    proper_divisors.push(1);
+    if n == 1 {
+        return proper_divisors;
+    } else if n_sqrt * n_sqrt == n {
+        proper_divisors.push(n_sqrt);
+    } else {
+        n_sqrt += 1;
+    }
+
+    for divisor_candidate in 2..n_sqrt {
+        if n % divisor_candidate == 0 {
+            proper_divisors.push(divisor_candidate);
+            proper_divisors.push(n / divisor_candidate);
+        }
+    }
+    proper_divisors
+}
