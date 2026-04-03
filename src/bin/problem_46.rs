bb8ae3dc77cded039a3d1372e64c9415f121268d
sch0rschi <getschii@gmail.com>
2026-04-03 14:09:46 +0200

    problem 46

---
+++ src/bin/problem_46.rs
@@ -0,0 +1,40 @@
+use projecteuler::primes::{primes_inclusive, Primes};
+use std::time::Instant;
+
+fn main() {
+    let start = Instant::now();
+
+    let mut upper = 1024;
+
+    loop {
+        upper *= 2;
+
+        let Primes { sieve, list } = primes_inclusive(upper);
+
+        for consecutive_prime_pair in list.windows(2) {
+            let [lower_prime, upper_prime] = consecutive_prime_pair else {
+                todo!()
+            };
+            for composite in ((*lower_prime + 2)..*upper_prime).step_by(2) {
+                let check = check_composite(composite, &sieve);
+                if !check {
+                    println!("{}", composite);
+                    println!("Elapsed: {:?}", start.elapsed());
+                    return;
+                }
+            }
+        }
+    }
+}
+
+fn check_composite(composite: i64, sieve: &Vec<bool>) -> bool {
+    let mut n = 1;
+    while 2 * n * n < composite {
+        let remainder = composite - 2 * n * n;
+        if sieve[remainder as usize] {
+            return true;
+        }
+        n += 1;
+    }
+    false
+}
