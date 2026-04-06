a8f079515e1f427fd947864eb40db0f55b018f07
sch0rschi <getschii@gmail.com>
2026-04-06 16:19:32 +0200

    problem 52

---
+++ src/bin/problem_52.rs
@@ -0,0 +1,50 @@
+use std::time::Instant;
+
+fn main() {
+    let start = Instant::now();
+    let mut from = 10000;
+
+    loop {
+        from *= 10;
+        let end = from * 10 / 6;
+
+        for i in (from + 8..=end).step_by(9) {
+            let base = get_digit_count_encoded(i);
+
+            if get_digit_count_encoded(2 * i) != base {
+                continue;
+            }
+
+            if get_digit_count_encoded(3 * i) != base {
+                continue;
+            }
+
+            if get_digit_count_encoded(4 * i) != base {
+                continue;
+            }
+
+            if get_digit_count_encoded(5 * i) != base {
+                continue;
+            }
+
+            if get_digit_count_encoded(6 * i) != base {
+                continue;
+            }
+
+            println!("{}", i);
+            println!("Elapsed: {:?}", start.elapsed());
+            return;
+        }
+    }
+}
+
+// 6 bits per digit allow the count up to 63
+fn get_digit_count_encoded(mut n: i32) -> u64 {
+    let mut sig = 0u64;
+    while n > 0 {
+        let digit = n % 10;
+        sig += 1 << (digit * 6);
+        n /= 10;
+    }
+    sig
+}
