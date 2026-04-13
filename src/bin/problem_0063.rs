179ddbe5ed8c7690a2526d106f8cb99fcf64fd03
sch0rschi <getschii@gmail.com>
2026-04-14 01:07:00 +0200

    simplify problem 63

--- src/bin/problem_0063.rs
+++ src/bin/problem_0063.rs
@@ -1,3 +1,4 @@
+use std::iter::successors;
 use std::time::Instant;
 
 fn main() {
@@ -11,38 +12,12 @@ fn main() {
 }
 
 fn solve_0063() -> u8 {
-    let mut count = 0;
-    let mut exponent = 1;
-    let mut smallest_target_power = 1;
-    let mut smallest_base_tracker = 1;
-    let mut previous_power = 1u128;
-
-    loop {
-        if let Some(smallest_valid_base) = find_smallest_valid_base(exponent, smallest_target_power, smallest_base_tracker, &mut previous_power) {
-            count += 10 - smallest_valid_base;
-            smallest_base_tracker = smallest_valid_base;
-            smallest_target_power *= 10;
-            exponent += 1;
-        } else {
-            break;
-        }
-    }
-
-    count
-}
-
-fn find_smallest_valid_base(exponent: u32, smallest_target_power: u128, smallest_base: u8, previous_power: &mut u128) -> Option<u8> {
-    let min_power = *previous_power * smallest_base as u128;
-    if min_power >= smallest_target_power {
-        *previous_power = min_power;
-        return Some(smallest_base);
-    }
-    for i in (smallest_base +1)..=9 {
-        let power = (i as u128).pow(exponent);
-        if power >= smallest_target_power {
-            *previous_power = power;
-            return Some(i);
-        }
-    }
-    None
+    (1..=9u128)
+        .flat_map(|base| {
+            successors(Some((base, 1u128)), move |&(p, low)| {
+                Some((p * base, low * 10))
+            })
+            .take_while(|&(power, low)| power >= low && power < 10 * low)
+        })
+        .count() as u8
 }
