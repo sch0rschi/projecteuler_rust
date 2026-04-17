0b6e50aee72672fead3025d074c5d4d73f6c32f1
sch0rschi <getschii@gmail.com>
2026-04-17 12:51:43 +0200

    problem 77

---
+++ src/bin/problem_0077.rs
@@ -0,0 +1,34 @@
+use projecteuler::primes::primes_inclusive;
+use std::time::Instant;
+
+static LIMIT: usize = 100;
+
+fn main() {
+    let start = Instant::now();
+    let result = solve_0077();
+    let duration = start.elapsed();
+    println!("{}", result);
+    println!("Elapsed: {:?}", duration);
+    assert_eq!(71, result);
+    assert!(duration < std::time::Duration::from_secs(1));
+}
+
+fn solve_0077() -> u64 {
+    let primes = primes_inclusive(LIMIT as u64);
+    let primes: Vec<usize> = primes.primes_list.iter().map(|&p| p as usize).collect();
+
+    let mut dp = vec![0u64; LIMIT + 1];
+    dp[0] = 1;
+
+    for &p in &primes {
+        for i in p..=LIMIT {
+            dp[i] += dp[i - p];
+        }
+        for (i, &item) in dp.iter().enumerate().take(p + 1).skip(1) {
+            if item > 5_000 {
+                return i as u64;
+            }
+        }
+    }
+    unreachable!()
+}
