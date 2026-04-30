acac61220bb7b95ea74dc949e35f25344e1a6ea6
sch0rschi <getschii@gmail.com>
2026-04-30 15:04:08 +0200

    improve problem 16

--- src/bin/problem_0016.rs
+++ src/bin/problem_0016.rs
@@ -1,16 +1,19 @@
-use num_bigint::BigInt;
+use num_bigint::BigUint;
 use projecteuler::evaluation_helper::solve_print_and_check;
 
 fn main() {
     solve_print_and_check(solve_0016, 1366);
 }
-fn solve_0016() -> i64 {
-    let base = BigInt::from(2);
-    let number = base.pow(1000);
-    number
-        .to_string()
-        .chars()
-        .map(|c| c.to_digit(10).unwrap() as i64)
-        .sum()
 
+fn solve_0016() -> u64 {
+    let mut n = BigUint::from(2u64);
+    n = n.pow(1000);
+
+    let mut sum = 0u64;
+
+    for byte in n.to_radix_be(10) {
+        sum += byte as u64;
+    }
+
+    sum
 }
