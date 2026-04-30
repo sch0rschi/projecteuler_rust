de798919d3c1f865d2d48406002a2fa1cf612027
sch0rschi <getschii@gmail.com>
2026-04-30 14:58:15 +0200

    improve problem 15

--- src/bin/problem_0015.rs
+++ src/bin/problem_0015.rs
@@ -1,17 +1,19 @@
 use projecteuler::evaluation_helper::solve_print_and_check;
 
-const N: usize = 20;
+const N: u64 = 20;
 
 fn main() {
     solve_print_and_check(solve_0015, 137846528820);
 }
-fn solve_0015() -> i64 {
-    let mut matrix: [[i64; N + 1]; N + 1] = [[1; N + 1]; N + 1];
-    for x in 1..=N {
-        for y in 1..=N {
-            matrix[x][y] = matrix[x - 1][y] + matrix[x][y - 1];
-        }
+
+fn solve_0015() -> u64 {
+    let mut result: u64 = 1;
+
+    // How many permutations of (R,R,R,R,...,D,D,D,D,...) are there?
+    // Safe binomial computation
+    for i in 1..=N {
+        result = result * (N + i) / (i);
     }
 
-    matrix[N][N]
+    result
 }
