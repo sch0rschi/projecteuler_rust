2ed52b880ede2d1a0d503b5edc226176ff438485
sch0rschi <getschii@gmail.com>
2026-04-30 16:14:35 +0200

    improve problem 21

--- src/bin/problem_0021.rs
+++ src/bin/problem_0021.rs
@@ -1,4 +1,3 @@
-use projecteuler::divisors::ProperDivisorSums;
 use projecteuler::evaluation_helper::solve_print_and_check;
 
 const LIMIT: usize = 10_000;
@@ -7,17 +6,21 @@ fn main() {
     solve_print_and_check(solve_0021, 31626);
 }
 
-fn solve_0021() -> u32 {
-    let divisor_sums = ProperDivisorSums::new(LIMIT);
-    let mut d: [u32; LIMIT] = [0; LIMIT];
-    let mut amicable_numbers_sum = 0;
-    for i in 1u32..LIMIT as u32 {
-        let proper_divisor_sum = divisor_sums.get(i);
-        d[i as usize] = proper_divisor_sum;
-        if proper_divisor_sum < i && d[proper_divisor_sum as usize] == i {
-            amicable_numbers_sum += i;
-            amicable_numbers_sum += proper_divisor_sum;
+fn solve_0021() -> usize {
+    let mut divisor_sums = [0usize; LIMIT];
+
+    for i in 1..LIMIT {
+        for j in (2 * i..LIMIT).step_by(i) {
+            divisor_sums[j] += i;
         }
     }
-    amicable_numbers_sum
+
+    divisor_sums
+        .iter()
+        .enumerate()
+        .filter(|&(i, &divisor_sum)| {
+            divisor_sum < LIMIT && divisor_sums[divisor_sum] == i && divisor_sum != i
+        })
+        .map(|(i, _)| i)
+        .sum()
 }
