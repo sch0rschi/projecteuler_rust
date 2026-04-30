b247852bf76d9c6c586f390af79b6cef272235f1
sch0rschi <getschii@gmail.com>
2026-04-30 16:01:08 +0200

    improve problem 5

--- src/bin/problem_0005.rs
+++ src/bin/problem_0005.rs
@@ -1,9 +1,20 @@
 use projecteuler::evaluation_helper::solve_print_and_check;
+use projecteuler::primes::Primes;
 
 fn main() {
     solve_print_and_check(solve_0005, 232792560);
 }
 
-fn solve_0005() -> u32 {
-    2 * 2 * 2 * 2 * 3 * 3 * 5 * 7 * 11 * 13 * 17 * 19
+const LIMIT: u64 = 20;
+
+fn solve_0005() -> u64 {
+    let mut result = 1u64;
+    let primes = Primes::primes_inclusive(LIMIT);
+
+    for p in primes.primes_list {
+        let exp = (LIMIT as f64).log(p as f64).floor() as u32;
+        result *= p.pow(exp);
+    }
+
+    result
 }
