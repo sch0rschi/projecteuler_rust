d97e143e0054a74c59d0853380665a39114dda6d
sch0rschi <getschii@gmail.com>
2026-04-30 15:16:29 +0200

    improve problem 19

--- src/bin/problem_0019.rs
+++ src/bin/problem_0019.rs
@@ -4,17 +4,19 @@ use projecteuler::evaluation_helper::solve_print_and_check;
 fn main() {
     solve_print_and_check(solve_0019, 171);
 }
-fn solve_0019() -> i32 {
-    let start = NaiveDate::from_ymd_opt(1901, 1, 1).unwrap();
-    let end = NaiveDate::from_ymd_opt(2000, 12, 31).unwrap();
 
-    let mut counter = 0;
-    let mut date = start;
-    while date <= end {
-        if date.day() == 1 && date.weekday() == Weekday::Sun {
-            counter += 1;
+fn solve_0019() -> i32 {
+    let mut count = 0;
+
+    for year in 1901..=2000 {
+        for month in 1..=12 {
+            let date = NaiveDate::from_ymd_opt(year, month, 1).unwrap();
+
+            if date.weekday() == Weekday::Sun {
+                count += 1;
+            }
         }
-        date = date.succ_opt().unwrap()
     }
-    counter
+
+    count
 }
