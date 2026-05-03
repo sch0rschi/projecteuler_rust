1f5dc4cb0f855db2ee48b776be10c8d6b71dece9
sch0rschi <getschii@gmail.com>
2026-05-04 01:13:35 +0200

    improve problem 28

--- src/bin/problem_0028.rs
+++ src/bin/problem_0028.rs
@@ -4,14 +4,22 @@ fn main() {
     solve_print_and_check(solve_0028, 669171001);
 }
 
-fn solve_0028() -> i32 {
-    let mut sum = 1;
-    let mut last = 1;
-    for i in 1..=500 {
-        let new_corner = last + 2 * i;
-        last = new_corner + 6 * i;
-        sum += 2 * new_corner;
-        sum += 2 * last;
-    }
-    sum
+const NUMBER_OF_RINGS: u32 = 500;
+
+// Four corners of ring k, summed over k=1..=n:
+// top-right:    Σ(2k+1)^2      = 4*Σk^2 + 4*Σk + n
+// top-left:     Σ(2k+1)^2 - 2k = 4*Σk^2 + 2*Σk + n
+// bottom-left:  Σ(2k+1)^2 - 4k = 4*Σk^2        + n
+// bottom-right: Σ(2k+1)^2 - 6k = 4*Σk^2 - 2*Σk + n
+fn solve_0028() -> u32 {
+
+    let sum_k = NUMBER_OF_RINGS * (NUMBER_OF_RINGS + 1) / 2;
+    let sum_k_squared = NUMBER_OF_RINGS * (NUMBER_OF_RINGS + 1) * (2 * NUMBER_OF_RINGS + 1) / 6;
+
+    let top_right = 4 * sum_k_squared + 4 * sum_k + NUMBER_OF_RINGS;
+    let top_left = 4 * sum_k_squared + 2 * sum_k + NUMBER_OF_RINGS;
+    let bottom_left = 4 * sum_k_squared + NUMBER_OF_RINGS;
+    let bottom_right = 4 * sum_k_squared - 2 * sum_k + NUMBER_OF_RINGS;
+
+    1 + top_right + top_left + bottom_left + bottom_right
 }
