82879422fd31bd1f415d33083c9d2e61eb1cef4c
sch0rschi <getschii@gmail.com>
2026-05-03 12:15:29 +0200

    improve problem 24

--- src/bin/problem_0024.rs
+++ src/bin/problem_0024.rs
@@ -1,29 +1,27 @@
+use num_integer::Integer;
 use projecteuler::evaluation_helper::solve_print_and_check;
-use projecteuler::factorials::get_factorial_array;
+
+const FAC_9: usize = 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2;
+const N_TH_PERMUTATION: usize = 1_000_000;
 
 fn main() {
-    solve_print_and_check(solve_0024, "2783915460".to_string());
+    solve_print_and_check(solve_0024, 2783915460);
 }
 
-fn solve_0024() -> String {
-    let mut permutation = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let permutation_len = permutation.len();
-    let number_of_possible_permutations: Vec<u64> =
-        get_factorial_array(10).iter().skip(1).copied().collect();
-    let mut n_th_permutation = 1_000_000 - 1;
-    while n_th_permutation > 0 {
-        let find = number_of_possible_permutations.binary_search(&n_th_permutation);
-        let find_sanitized = find.unwrap_or_else(|i| i - 1);
-        let number_of_possible_permutations = number_of_possible_permutations[find_sanitized];
-        let offset = n_th_permutation / number_of_possible_permutations;
-        let decrease = (offset) * number_of_possible_permutations;
-        n_th_permutation -= decrease;
-        let swap_index = permutation_len - 2 - find_sanitized;
-        permutation.swap(swap_index, swap_index + offset as usize);
-        permutation[swap_index + 1..].sort();
+fn solve_0024() -> usize {
+    let mut digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let mut result = 0;
+    let mut div: usize;
+    let mut remaining = N_TH_PERMUTATION - 1;
+    let mut number_of_open_digits = digits.len();
+    let mut permutation_range = FAC_9;
+
+    for _ in 0..9 {
+        (div, remaining) = remaining.div_rem(&permutation_range);
+        result = result * 10 + digits[div];
+        digits.copy_within(div + 1..number_of_open_digits, div);
+        number_of_open_digits -= 1;
+        permutation_range /= number_of_open_digits;
     }
-    permutation
-        .iter()
-        .map(|x| x.to_string())
-        .collect::<String>()
+    result * 10 + digits[0]
 }
