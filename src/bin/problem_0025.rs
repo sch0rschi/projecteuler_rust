715272b1af852e3cb47ee02e2aa93f7feb3f9028
sch0rschi <getschii@gmail.com>
2026-05-03 17:04:56 +0200

    improve problem 25

--- src/bin/problem_0025.rs
+++ src/bin/problem_0025.rs
@@ -1,23 +1,18 @@
-use num_bigint::BigInt;
-use num_traits::One;
 use projecteuler::evaluation_helper::solve_print_and_check;
-use std::ops::Add;
 
 fn main() {
     solve_print_and_check(solve_0025, 4782);
 }
 
+// https://en.wikipedia.org/wiki/Fibonacci_sequence#Computation_by_rounding
+// solve for F_n = 10^1000 by log both sides
 fn solve_0025() -> i32 {
-    let mut f_p = BigInt::one();
-    let mut f_n = BigInt::one();
-    let mut n_th = 2;
+    let target_digits = 1000.0;
 
-    while f_n < BigInt::from(10).pow(999) {
-        let temp = f_n.clone();
-        f_n = f_n.add(f_p);
-        f_p = temp;
-        n_th += 1;
-    }
+    let sqrt5 = 5f64.sqrt();
+    let phi = (1.0 + sqrt5) / 2.0;
 
-    n_th
+    let n = ((target_digits - 1.0 + sqrt5.log10() / 2.0) / phi.log10()).ceil();
+
+    n as i32
 }
