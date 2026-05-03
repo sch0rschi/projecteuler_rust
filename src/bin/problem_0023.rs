9e79457e787c4591657672b5a4b6ca8002d698a6
sch0rschi <getschii@gmail.com>
2026-05-03 11:03:44 +0200

    improve problem 23

--- src/bin/problem_0023.rs
+++ src/bin/problem_0023.rs
@@ -1,39 +1,44 @@
 use projecteuler::evaluation_helper::solve_print_and_check;
 
+const LIMIT: usize = 28123;
+
 fn main() {
     solve_print_and_check(solve_0023, 4179871);
 }
 
 fn solve_0023() -> usize {
-    let limit = 28123;
+    let divisor_sums = compute_divisor_sums();
 
-    let divisor_sums = compute_divisor_sums(limit);
+    let mut is_abundant = [false; LIMIT + 1];
+    let mut abundants = Vec::with_capacity(7000);
 
-    let abundant: Vec<usize> = (2..=limit).filter(|&i| divisor_sums[i] > i).collect();
-
-    let mut can_be_written = vec![false; limit + 1];
-
-    for (i, &n1) in abundant.iter().enumerate() {
-        for &n2 in &abundant[i..] {
-            let sum = n1 + n2;
-            if sum > limit {
-                break;
-            }
-            can_be_written[sum] = true;
+    for i in 2..=LIMIT {
+        if divisor_sums[i] > i {
+            is_abundant[i] = true;
+            abundants.push(i);
         }
     }
 
-    (1..=limit)
-        .filter(|&i| !can_be_written[i])
-        .sum()
-
+    (1..LIMIT).filter(|n| !can_be_written_as_sum(&is_abundant, &abundants, *n)).sum()
 }
 
-fn compute_divisor_sums(limit: usize) -> Vec<usize> {
-    let mut sums = vec![0; limit + 1];
+fn can_be_written_as_sum(is_abundant: &[bool; 28124], abundants: &[usize], n: usize) -> bool {
+    for &a in abundants {
+        if a > n / 2 {
+            return false;
+        }
+        if is_abundant[n - a] {
+            return true;
+        }
+    }
+    false
+}
 
-    for i in 1..=limit / 2 {
-        for j in (2 * i..=limit).step_by(i) {
+fn compute_divisor_sums() -> Vec<usize> {
+    let mut sums = vec![0; LIMIT + 1];
+
+    for i in 1..=LIMIT / 2 {
+        for j in (2 * i..=LIMIT).step_by(i) {
             sums[j] += i;
         }
     }
