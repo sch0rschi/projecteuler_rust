0cc201beec00d0810a3ad20cf1707c7677d33443
sch0rschi <getschii@gmail.com>
2026-05-07 00:08:05 +0200

    improve problem 4

--- src/bin/problem_0004.rs
+++ src/bin/problem_0004.rs
@@ -1,19 +1,23 @@
-use std::ops::RangeInclusive;
 use num_integer::Roots;
 use projecteuler::evaluation_helper::solve_print_and_check;
+use std::ops::RangeInclusive;
 
 fn main() {
     solve_print_and_check(solve_0004, 906609);
 }
 
 fn solve_0004() -> u32 {
-    RangeInclusive::new(100, 999).rev().map(make_palindrome).find(|&p| is_valid_product(p)).unwrap()
+    RangeInclusive::new(100, 999)
+        .rev()
+        .map(make_palindrome)
+        .find(|&p| is_valid_product(p))
+        .unwrap()
 }
 
 fn is_valid_product(palindrome: u32) -> bool {
     let lower = (palindrome / 999).max(palindrome.sqrt());
     let upper = (palindrome / 100).min(999);
-    for divisor in lower..=upper {
+    for divisor in lower..upper + 1 {
         if palindrome.is_multiple_of(divisor) {
             return true;
         }
