8b179d0c5069d881681889263dc7d2b57373e9e9
sch0rschi <getschii@gmail.com>
2026-05-09 10:12:52 +0200

    improve problem 36

--- src/bin/problem_0036.rs
+++ src/bin/problem_0036.rs
@@ -1,4 +1,4 @@
-use projecteuler::digits::{get_digits, get_digits_in_binary};
+use num_integer::Integer;
 use projecteuler::evaluation_helper::solve_print_and_check;
 
 fn main() {
@@ -7,20 +7,45 @@ fn main() {
 
 fn solve_0036() -> u64 {
     let mut sum = 0;
-    for i in 1..1_000_000 {
-        let digits = get_digits(i);
-        if digits == digits.iter().rev().copied().collect::<Vec<u64>>() {
-            let digits_in_binary = get_digits_in_binary(i);
-            if digits_in_binary
-                == digits_in_binary
-                    .iter()
-                    .rev()
-                    .copied()
-                    .collect::<Vec<bool>>()
-            {
-                sum += i;
-            }
+    for i in 1..999 {
+        let p1 = make_odd_palindrome(i);
+        let p2 = make_even_palindrome(i);
+        if is_binary_palindrome(p1) {
+            sum += p1;
+        }
+        if is_binary_palindrome(p2) {
+            sum += p2;
         }
     }
     sum
+}
+
+fn make_even_palindrome(mut n: u64) -> u64 {
+    let mut result = n;
+    let mut rem;
+    while n > 0 {
+        (n, rem) = n.div_rem(&10);
+        result = result * 10 + rem;
+    }
+    result
+}
+
+fn make_odd_palindrome(mut n: u64) -> u64 {
+    let mut result = n;
+    n /= 10;
+    let mut rem;
+    while n > 0 {
+        (n, rem) = n.div_rem(&10);
+        result = result * 10 + rem;
+    }
+    result
+}
+
+fn is_binary_palindrome(n: u64) -> bool {
+    if n.is_multiple_of(2) {
+        return false;
+    }
+    let reversed = n.reverse_bits();
+    let leading_zeros = n.leading_zeros();
+    reversed >> leading_zeros == n
 }
