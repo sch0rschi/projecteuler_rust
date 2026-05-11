ecfe80f9ea17957dee8ea7e7a5822925a9bed04c
sch0rschi <getschii@gmail.com>
2026-05-11 16:29:58 +0200

    improve problem 74

--- src/solutions/problem_0074.rs
+++ src/solutions/problem_0074.rs
@@ -1,21 +1,26 @@
-const FACTORIALS: [u64; 10] = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
+const FACTORIALS: [u32; 10] = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
 
+const LIMIT: usize = 2_600_000;
 
 pub fn solve_0074() -> i32 {
     let mut chain_count = 0;
+    let mut loop_length_map = vec![0u8; LIMIT];
+    let mut seen = vec![false; LIMIT];
+    let mut chain_list: Vec<u32> = Vec::with_capacity(64);
 
-    let mut loop_length_map = vec![0u64; 2_600_000];
-    let mut seen = vec![false; 2_600_000];
-    let mut chain_list: Vec<u64> = Vec::with_capacity(64);
+    let mut next_cache = vec![0u32; LIMIT];
+    for n in 1..LIMIT {
+        next_cache[n] = next_cache[n / 10] + FACTORIALS[n % 10];
+    }
 
-    for n in 1..=1_000_000 {
+    for n in 1u32..=1_000_000 {
         chain_list.clear();
 
         let mut next = n;
 
         loop {
             if loop_length_map[next as usize] != 0 {
-                let total_len = chain_list.len() as u64 + loop_length_map[next as usize];
+                let total_len = chain_list.len() as u8 + loop_length_map[next as usize];
                 if total_len == 60 {
                     chain_count += 1;
                 }
@@ -25,7 +30,7 @@ pub fn solve_0074() -> i32 {
             if seen[next as usize] {
                 let pos = chain_list.iter().position(|&x| x == next).unwrap();
 
-                let loop_length = (chain_list.len() - pos) as u64;
+                let loop_length = (chain_list.len() - pos) as u8;
 
                 for &loop_element in &chain_list[pos..] {
                     loop_length_map[loop_element as usize] = loop_length;
@@ -41,7 +46,7 @@ pub fn solve_0074() -> i32 {
             seen[next as usize] = true;
             chain_list.push(next);
 
-            next = sum_of_digit_factorials(next);
+            next = next_cache[next as usize];
         }
 
         for &x in &chain_list {
@@ -50,14 +55,6 @@ pub fn solve_0074() -> i32 {
     }
 
     chain_count
-}
-fn sum_of_digit_factorials(mut n: u64) -> u64 {
-    let mut sum = 0;
-    while n > 0 {
-        sum += FACTORIALS[n as usize % 10];
-        n /= 10;
-    }
-    sum
 }
 
 #[cfg(test)]
