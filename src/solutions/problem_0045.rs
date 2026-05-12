a31500ea6c80418ddff58a2be703ef32bf20e118
sch0rschi <getschii@gmail.com>
2026-05-12 23:30:17 +0200

    improve problem 45

--- src/solutions/problem_0045.rs
+++ src/solutions/problem_0045.rs
@@ -1,26 +1,25 @@
-pub fn solve_0045() -> i32 {
+pub fn solve_0045() -> i64 {
     // all hexagonal numbers are also triangle numbers, hence ignoring triangle numbers
 
     // Starting after known solution 40755:
     // P(165) = H(143) = 40755
 
-    let mut p_j = 40755;
-    let mut j = 165;
-
-    let mut h_k = 40755;
-    let mut k = 143;
+    let mut hi = 144i64;
+    let mut pi = 165i64;
+    let mut h = hi * (2 * hi - 1);
+    let mut p = pi * (3 * pi - 1) / 2;
 
     loop {
-        if p_j <= h_k {
-            p_j += 3 * j + 1;
-            j += 1;
-        } else {
-            h_k += 4 * k + 1;
-            k += 1;
-        }
-
-        if p_j == h_k {
-            return p_j;
+        match p.cmp(&h) {
+            std::cmp::Ordering::Less => {
+                pi += 1;
+                p = pi * (3 * pi - 1) / 2;
+            }
+            std::cmp::Ordering::Greater => {
+                hi += 1;
+                h = hi * (2 * hi - 1);
+            }
+            std::cmp::Ordering::Equal => return p,
         }
     }
 }
