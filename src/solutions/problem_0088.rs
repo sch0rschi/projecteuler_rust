1470467bb33c3ba5682d91ac05611657a1597f03
sch0rschi <getschii@gmail.com>
2026-05-12 09:34:39 +0200

    improve problem 88

--- src/solutions/problem_0088.rs
+++ src/solutions/problem_0088.rs
@@ -1,38 +1,40 @@
-use crate::libs::multiset_partitions::MultisetPartitions;
-use crate::libs::primes::Primes;
 use itertools::Itertools;
 
 const LIMIT: usize = 12_000;
 
 
 pub fn solve_0088() -> u64 {
-    let primes = Primes::primes_inclusive(2 * LIMIT as u64);
-    let mut min_product_sum: [u64; LIMIT + 1] = [0u64; LIMIT + 1];
-    let mut count = 1;
-    for i in 4u64.. {
-        let prime_factorization = primes.prime_factors(i);
+    let mut min_product_sum = vec![u64::MAX; LIMIT + 1];
 
-        let multiset_partitions = MultisetPartitions::new(prime_factorization);
-        for multiset_partition in multiset_partitions {
-            if multiset_partition.blocks.len() == 1 {
-                continue;
-            }
-            let product_sum: usize = multiset_partition
-                .blocks
-                .iter()
-                .map(|block: &Vec<u64>| block.iter().product::<u64>() as usize)
-                .sum();
-            let k = multiset_partition.blocks.len() + i as usize - product_sum;
-            if k <= LIMIT && min_product_sum[k] == 0 {
-                count += 1;
-                min_product_sum[k] = i;
-                if count == LIMIT {
-                    return min_product_sum.iter().unique().sum();
-                }
-            }
+    search(1, 1, 1, 2, &mut min_product_sum);
+    min_product_sum[2..=LIMIT].iter().unique().sum()
+}
+
+fn search(
+    product: usize,
+    sum: usize,
+    terms: usize,
+    min_factor: usize,
+    min_product_sum: &mut Vec<u64>,
+) {
+    let k = terms + product - sum;
+    if k > LIMIT {
+        return;
+    }
+
+    if terms >= 1 {
+        if (product as u64) < min_product_sum[k] {
+            min_product_sum[k] = product as u64;
         }
     }
-    unreachable!()
+
+    for f in min_factor..=LIMIT {
+        let new_product = product * f;
+        if new_product > 2 * LIMIT {
+            break;
+        }
+        search(new_product, sum + f, terms + 1, f, min_product_sum);
+    }
 }
 
 #[cfg(test)]
