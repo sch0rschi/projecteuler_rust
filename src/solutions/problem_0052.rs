1a2e45bb747229ee6a779d7dd57221066feba8a4
sch0rschi <getschii@gmail.com>
2026-05-18 12:15:58 +0200

    improve problem 52

--- src/solutions/problem_0052.rs
+++ src/solutions/problem_0052.rs
@@ -1,4 +1,6 @@
-pub fn solve_0052() -> i32 {
+use crate::libs::digits::get_digit_count_encoding_15_max;
+
+pub fn solve_0052() -> u64 {
     let mut from = 10000;
 
     loop {
@@ -6,42 +8,31 @@ pub fn solve_0052() -> i32 {
         let end = from * 10 / 6;
 
         for i in (from + 8..=end).step_by(9) {
-            let base = get_digit_count_encoded(i);
+            let base = get_digit_count_encoding_15_max(i);
 
-            if get_digit_count_encoded(2 * i) != base {
+            if get_digit_count_encoding_15_max(2 * i) != base {
                 continue;
             }
 
-            if get_digit_count_encoded(3 * i) != base {
+            if get_digit_count_encoding_15_max(3 * i) != base {
                 continue;
             }
 
-            if get_digit_count_encoded(4 * i) != base {
+            if get_digit_count_encoding_15_max(4 * i) != base {
                 continue;
             }
 
-            if get_digit_count_encoded(5 * i) != base {
+            if get_digit_count_encoding_15_max(5 * i) != base {
                 continue;
             }
 
-            if get_digit_count_encoded(6 * i) != base {
+            if get_digit_count_encoding_15_max(6 * i) != base {
                 continue;
             }
 
             return i;
         }
     }
-}
-
-// 6 bits per digit allow the count up to 63
-fn get_digit_count_encoded(mut n: i32) -> u64 {
-    let mut sig = 0u64;
-    while n > 0 {
-        let digit = n % 10;
-        sig += 1 << (digit * 6);
-        n /= 10;
-    }
-    sig
 }
 
 #[cfg(test)]
