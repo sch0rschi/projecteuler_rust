4945d53ea68c2a1576034c9b3c25ec737a8d829a
sch0rschi <getschii@gmail.com>
2026-05-18 21:08:11 +0200

    improve problem 80

--- src/solutions/problem_0080.rs
+++ src/solutions/problem_0080.rs
@@ -1,61 +1,25 @@
-use num_bigint::BigInt;
-use num_integer::Roots;
+use num_bigint::BigUint;
 
+pub fn solve_0080() -> u32 {
+    let ten_pow_198 = BigUint::from(10u32).pow(198);
 
-pub fn solve_0080() -> u64 {
-    (2..=100)
-        .filter(|&i| {
-            let sqrt = i.sqrt();
-            sqrt * sqrt != i
+    (2u32..100)
+        .filter(|&n| {
+            let r = (n as f64).sqrt() as u32;
+            r * r != n
         })
-        .map(decimal_digit_sum_100)
+        .map(|n| get_root_decimal_digit_sum(n, &ten_pow_198))
         .sum()
 }
 
-fn decimal_digit_sum_100(n: u64) -> u64 {
-    let ten = BigInt::from(10u32);
-    let hundred = BigInt::from(100u32);
-
-    let mut sqrt_scale = BigInt::from(n.isqrt());
-    let mut n_scale = BigInt::from(n);
-
-    for _ in 0..=100 {
-        sqrt_scale *= &ten;
-        n_scale *= &hundred;
-
-        let digit = best_digit(&sqrt_scale, &n_scale);
-
-        sqrt_scale += digit;
-    }
-
-    sqrt_scale
-        .to_string()
-        .chars()
+fn get_root_decimal_digit_sum(n: u32, ten_pow_198: &BigUint) -> u32 {
+    (BigUint::from(n) * ten_pow_198)
+        .sqrt()
+        .to_radix_be(10)
+        .iter()
         .take(100)
-        .map(|c| (c as u8 - b'0') as u64)
+        .map(|&d| d as u32)
         .sum()
-}
-
-fn best_digit(sqrt_scale: &BigInt, n_scale: &BigInt) -> u32 {
-    let mut low = 0i32;
-    let mut high = 9i32;
-    let mut best = 0u32;
-
-    while low <= high {
-        let mid = (low + high) / 2;
-
-        let candidate = sqrt_scale + mid;
-        let square = &candidate * &candidate;
-
-        if square <= *n_scale {
-            best = mid as u32;
-            low = mid + 1;
-        } else {
-            high = mid - 1;
-        }
-    }
-
-    best
 }
 
 #[cfg(test)]
